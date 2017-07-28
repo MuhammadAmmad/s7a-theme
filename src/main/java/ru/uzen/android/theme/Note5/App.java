524741a2eb0260e44a4e0ee2f1ee8762a6f14397
anna <anna@localhost.localdomain>
2017-07-28 16:09:51 +0500

    init

---
+++ src/main/java/ru/uzen/android/theme/Note5/App.java
@@ -0,0 +1,11 @@
+package ru.uzen.android.theme.Note5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class App extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
